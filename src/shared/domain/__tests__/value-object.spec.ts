43a89f95f4d65d037b6dbfea0706e215ae105e19
Tiago Behenck <tiago.behenck@cwi.com.br>
2023-09-13 18:59:01 +0000

    create the value object

---
+++ src/shared/domain/__tests__/value-object.spec.ts
@@ -0,0 +1,44 @@
+import { ValueObject } from '../value-object';
+
+class StringValueObject extends ValueObject { 
+  constructor(readonly value: string) { 
+    super();
+  }
+}
+
+class ComplexValueObject extends ValueObject { 
+  constructor(readonly prop1: string, readonly prop2: number) { 
+    super();
+  }
+}
+
+describe('ValueObject Unit Tests', () => { 
+  test('should be equals', () => { 
+    const valueObject1 = new StringValueObject('test');
+    const valueObject2 = new StringValueObject('test');
+
+    expect(valueObject1.equals(valueObject2)).toBeTruthy();
+
+    const complexObject1 = new ComplexValueObject('test', 1);
+    const complexObject2 = new ComplexValueObject('test', 1);
+
+    expect(complexObject1.equals(complexObject2)).toBeTruthy();
+  })
+
+  test('should not be equals', () => { 
+    const valueObject1 = new StringValueObject('test');
+    const valueObject2 = new StringValueObject('test2');
+
+    expect(valueObject1.equals(valueObject2)).toBeFalsy();
+    expect(valueObject1.equals(null as any)).toBeFalsy();
+    expect(valueObject1.equals(undefined as any)).toBeFalsy();
+     
+
+    const complexObject1 = new ComplexValueObject('test', 1);
+    const complexObject2 = new ComplexValueObject('test', 2);
+
+    expect(complexObject1.equals(complexObject2)).toBeFalsy();
+    expect(complexObject1.equals(null as any)).toBeFalsy();
+    expect(complexObject2.equals(undefined as any)).toBeFalsy();
+  })
+})
