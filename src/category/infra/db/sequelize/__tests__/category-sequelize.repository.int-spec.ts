b5879736d3785a19cd9b4b69b10937ea14068738
Tiago Behenck <tiago.behenck@cwi.com.br>
2023-11-14 18:01:15 +0000

    add more tests

--- src/category/infra/db/sequelize/__tests__/category-sequelize.repository.int-spec.ts
+++ src/category/infra/db/sequelize/__tests__/category-sequelize.repository.int-spec.ts
@@ -2,10 +2,12 @@ import { Sequelize } from 'sequelize-typescript'
 import { CategoryModel } from '../category.model'
 import { CategorySequelizeRepository } from '../category-sequelize.repository'
 import { Category } from '../../../../domain/category.entity'
+import { Uuid } from '../../../../../shared/domain/value-objects/uuid.vo'
+import { NotFoundError } from '../../../../../shared/domain/errors/not-found.error'
 
-describe('Category Sequelize Respository Integration Test', () => { 
+describe('Category Sequelize Repository Integration it', () => { 
   let sequelize
-  let respository: CategorySequelizeRepository
+  let repository: CategorySequelizeRepository
 
   beforeEach(async () => { 
     sequelize = new Sequelize({
@@ -15,21 +17,76 @@ describe('Category Sequelize Respository Integration Test', () => {
     });
 
     await sequelize.sync({ force: true });
-    respository = new CategorySequelizeRepository(CategoryModel);
+    repository = new CategorySequelizeRepository(CategoryModel);
   })
 
-  test('should insert a new category', async () => { 
-    const category = Category.fake().aCategory().build();
-    await respository.insert(category);
+  it('should insert a new entity', async () => { 
+    const entity = Category.fake().aCategory().build();
+    await repository.insert(entity);
 
-    const model = CategoryModel.findByPk(category.category_id.id);
+    const model = await CategoryModel.findByPk(entity.category_id.id);
 
     expect(model.toJSON()).toMatchObject({
-      category_id: category.category_id.id,
-      name: category.name,
-      description: category.description,
-      is_active: category.is_active,
-      created_at: category.created_at,
+      category_id: entity.category_id.id,
+      name: entity.name,
+      description: entity.description,
+      is_active: entity.is_active,
+      created_at: entity.created_at,
     })
   })
-})
+
+  it('should finds a entity by id', async () => { 
+    let entityFound = await repository.findById(new Uuid());
+    expect(entityFound).toBeNull();
+
+    const entity = Category.fake().aCategory().build();
+    await repository.insert(entity);
+    entityFound = await repository.findById(entity.category_id);
+    expect(entity.toJSON()).toStrictEqual(entityFound.toJSON());
+  })
+
+  it('should return all categories', async () => { 
+    const entity = Category.fake().theCategories(10).build();
+    await repository.bulkInsert(entity);
+
+    const entities = await CategoryModel.findAll()
+
+    expect(entities.length).toBe(entity.length);
+    expect(JSON.stringify(entities)).toBe(JSON.stringify(entity))
+  })
+
+  it('should throw error on update when a entity not found', async () => { 
+    const entity = Category.fake().aCategory().build();
+    await expect(repository.update(entity)).rejects.toThrow(
+      new NotFoundError(entity.category_id.id, Category)
+    );
+  })
+
+  it('should update a entity', async () => {
+    const entity = Category.fake().aCategory().build();
+    await repository.insert(entity);
+
+    entity.changeName('Change name');
+    await repository.update(entity);
+
+    const entityFound = await repository.findById(entity.category_id);
+    expect(entity.toJSON()).toStrictEqual(entityFound.toJSON());
+  })
+
+  it('should throw error on delete when a entity not found', async () => { 
+    const entity = Category.fake().aCategory().build();
+    await expect(repository.delete(entity.category_id)).rejects.toThrow(
+      new NotFoundError(entity.category_id.id, Category)
+    );
+  })
+
+  it('should delete a entity', async () => { 
+    const entity = Category.fake().aCategory().build();
+    await repository.insert(entity);
+
+    await repository.delete(entity.category_id);
+
+    const entityFound = await repository.findById(entity.category_id);
+    expect(entityFound).toBeNull();
+  })
+})  
