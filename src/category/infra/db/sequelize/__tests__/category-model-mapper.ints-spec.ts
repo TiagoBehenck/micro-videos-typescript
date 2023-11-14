90a4d7305164d839b3366c7a3aa8d1ef5693c7d9
Tiago Behenck <tiago.behenck@cwi.com.br>
2023-11-14 21:29:27 +0000

    create the category model mapper with tests

---
+++ src/category/infra/db/sequelize/__tests__/category-model-mapper.ints-spec.ts
@@ -0,0 +1,64 @@
+import { Sequelize } from 'sequelize-typescript'
+import { CategoryModel } from '../category.model'
+import { CategoryModelMapper } from '../category-model-mapper'
+import { EntityValidationError } from '../../../../../shared/domain/validators/validation.error'
+import { Category } from '../../../../domain/category.entity'
+import { Uuid } from '../../../../../shared/domain/value-objects/uuid.vo'
+
+describe('Category Model Mapper Integration it', () => { 
+  let sequelize
+
+  beforeEach(async () => { 
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      models: [CategoryModel],
+      logging: false,
+    });
+
+    await sequelize.sync({ force: true });
+  })
+
+  test('should throws error when category is invalid', async () => { 
+    const model = CategoryModel.build({
+      category_id: '35d12ea6-6d18-49a9-9842-c54748e610e2',
+    })
+
+    try { 
+      CategoryModelMapper.toEntity(model);
+      fail('The category is valid, but it needs throws a EntityValidationError');
+    } catch(e) { 
+      expect(e).toBeInstanceOf(EntityValidationError);
+      expect((e as EntityValidationError).error).toMatchObject(
+        {
+          name: [
+            "name should not be empty",
+            "name must be a string",
+            "name must be shorter than or equal to 255 characters"
+          ],
+        }
+      )
+    }
+  })
+
+  test('should convert a category aggregate to a category model', () => { 
+    const created_at = new Date();
+    const aggregate = new Category({ 
+      category_id: new Uuid('35d12ea6-6d18-49a9-9842-c54748e610e2'),
+      name: 'some name',
+      description: 'some description',
+      is_active: true,
+      created_at,
+    })
+
+    const model = CategoryModelMapper.toModel(aggregate);
+
+    expect(model.toJSON()).toStrictEqual({ 
+      category_id: '35d12ea6-6d18-49a9-9842-c54748e610e2',
+      name: 'some name',
+      description: 'some description',
+      is_active: true,
+      created_at,
+    })
+  })
+})  
